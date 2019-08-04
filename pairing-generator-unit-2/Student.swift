714ba0d61a54ec0ee40c344e92b6d0ea64914beb
davidlawrencer <davidlrifkin+github@gmail.com>
2019-08-04 10:16:03 -0400

    Add Student

---
+++ pairing-generator-unit-2/Student.swift
@@ -0,0 +1,32 @@
+//
+//  Student.swift
+//  pairing-generator-unit-2
+//
+//  Created by David Rifkin on 7/30/19.
+//  Copyright © 2019 David Rifkin. All rights reserved.
+//
+
+import Foundation
+
+struct Student {
+    let name: String
+    let image: String
+    let pastMatches: [String]
+    
+//    func pickMatch() -> Student {
+//        while true {
+//            let randomStudent = getRandomStudent()
+//
+//            if checkIfAlreadyPaired(currentPerson: self,potentialMatch: randomStudent.name){
+//                continue
+//            } else {
+//                return randomStudent
+//            }
+//        }
+//    }
+    
+    func seeIfAlreadyPaired(potentialMatch name: String) -> Bool {
+        return self.pastMatches.contains(name)
+    }
+
+}
