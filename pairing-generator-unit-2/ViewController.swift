44f9a7c3c43839c4e6508f390915c07058e3773c
davidlawrencer <davidlrifkin+github@gmail.com>
2019-08-04 10:14:18 -0400

    Initial Commit

---
+++ pairing-generator-unit-2/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  pairing-generator-unit-2
+//
+//  Created by David Rifkin on 7/30/19.
+//  Copyright © 2019 David Rifkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var topPairName: UILabel!
+    @IBOutlet weak var bottomPairName: UILabel!
+    @IBOutlet weak var bottomPairImage: UIImageView!
+    @IBOutlet weak var topPairImage: UIImageView!
+    
+    @IBAction func gimmeNewPairButtonPressed(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
